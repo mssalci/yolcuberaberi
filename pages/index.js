ab948843b8507e0ab3922bb733212aef1c8cc0e4
Mustafa Serdar ŞALCI <mssalci@gmail.com>
2025-04-20 01:32:55 +0300

    Ana sayfa eklendi

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Yolcu Beraberi</h1>
+      <p>Yurt dışından parça ve eşya getirme platformu yayında!</p>
+    </div>
+  );
+}
