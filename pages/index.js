7b690836fb756802513782aa88c977f27b96455d
Mustafa Serdar ŞALCI <mssalci@gmail.com>
2025-04-20 23:17:34 +0300

    Ana sayfa güncellendi

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
       <h1>Yolcu Beraberi</h1>
-      <p>Yurtdışından parça veya eşya getirmek isteyenlerle, yurtdışından gelen yolcuları buluşturan platform.</p>
+      <p>Yurtdışından parça veya eşya getirmek isteyenlerle, yurtdışından gelen yolcuları buluşturan İLK ve TEK platformdur.</p>
     </div>
   );
 }
