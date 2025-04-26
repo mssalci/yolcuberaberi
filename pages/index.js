dec9eecd4e813b74e5938d00f04d00f89b72e49c
mssalci <serdarsalci@hotmail.com>
2025-04-27 00:06:49 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Header from '../components/Header';
 
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Header />
+      {/* Diğer içerikler buraya */}
+    </div>
+  );
+}
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
