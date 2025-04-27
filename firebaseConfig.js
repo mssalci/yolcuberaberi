46d27d21e8a216d48eb83716e056c1eb1ae8239a
mssalci <serdarsalci@hotmail.com>
2025-04-27 20:19:16 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,27 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "senin-apiKey-buraya",
-  authDomain: "senin-authDomain-buraya",
-  projectId: "senin-projectId-buraya",
-  storageBucket: "senin-storageBucket-buraya",
-  messagingSenderId: "senin-messagingSenderId-buraya",
-  appId: "senin-appId-buraya"
+  apiKey: "AIzaSyCk0Ib2BGW6wLEzYM9z5Ry6j7HdSNugP7c",
+
+
+  authDomain: "yolcuberaberi-9e3c7.firebaseapp.com",
+
+
+  projectId: "yolcuberaberi-9e3c7",
+
+
+  storageBucket: "yolcuberaberi-9e3c7.firebasestorage.app",
+
+
+  messagingSenderId: "877346669639",
+
+
+  appId: "1:877346669639:web:e39ddce5d01cfd06fb5d62",
+
+
+  measurementId: "G-JXMW577W04"
+
+
 };
 
 // Uygulamayı başlat
