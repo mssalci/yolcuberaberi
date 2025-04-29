05ff2266052a1f498fd5c8334acf7e12b0b1fb9d
mssalci <serdarsalci@hotmail.com>
2025-04-29 07:30:54 +0300

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,8 +1,8 @@
 // firebase/firebaseConfig.js
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCk0Ib2BGW6wLEzYM9z5Ry6j7HdSNugP7c",
@@ -13,7 +13,8 @@ const firebaseConfig = {
   appId: "1:877346669639:android:8caa171a53755b00fb5d62"
 };
 
-const app = initializeApp(firebaseConfig);
+// Firebase'i tekrar tekrar initialize etmeyi önle
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 const auth = getAuth(app);
 const db = getFirestore(app);
