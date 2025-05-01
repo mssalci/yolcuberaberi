e15ac97bf5b8538509d5efe1d628c2630efd1f65
mssalci <serdarsalci@hotmail.com>
2025-05-01 08:39:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 // pages/_app.js
-import "../styles/globals.css";
-import Header from "../components/Header";
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Header />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
-
-export default MyApp;
