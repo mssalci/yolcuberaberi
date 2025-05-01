df37750e65a46248d8cfce1f87b1c120b6091230
mssalci <serdarsalci@hotmail.com>
2025-05-01 08:39:54 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Layout from '../components/Layout';
+// pages/index.js
 
 export default function HomePage() {
   return (
-    <Layout>
+    <>
       <h1 className="text-3xl font-bold mb-4 text-gray-800">Hoş Geldiniz!</h1>
       <p className="text-gray-700">
         Soldaki menüden talepleri görüntüleyebilir, yeni talepler oluşturabilir veya eşleşmeleri yönetebilirsiniz.
       </p>
-    </Layout>
+    </>
   );
 }
