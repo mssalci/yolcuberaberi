cb9b0573171721522806a15a9c0eb36667bacc5a
mssalci <serdarsalci@hotmail.com>
2025-05-02 22:20:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 // pages/_app.js
-import '../styles/globals.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+ import '../styles/globals.css';
+ import Header from '../components/Header';
+ 
+ export default function MyApp({ Component, pageProps }) {
+   return (
+   );
+   return <Component {...pageProps} />;
+     }
