e97082e0d4b21c9892835e03f3527ce4c267e352
mssalci <serdarsalci@hotmail.com>
2025-05-02 23:56:12 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 // pages/_app.js
- import '../styles/globals.css';
- import Header from '../components/Header';
- 
- export default function MyApp({ Component, pageProps }) {
-  
-   return <Component {...pageProps} />;
-     }
+import '../styles/globals.css';
+import Header from '../components/Header';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
