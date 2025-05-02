ffcbb3dee041fdd7cffdabcfe912cd07b5c6a16c
mssalci <serdarsalci@hotmail.com>
2025-05-02 22:11:08 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,6 @@
+// pages/_app.js
 import '../styles/globals.css';
-import Header from '../components/Header';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen bg-gray-50">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
