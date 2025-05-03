16e87e5d63a19bf1852914f0300ff17cf0563aa7
mssalci <serdarsalci@hotmail.com>
2025-05-03 12:57:24 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 // pages/_app.js
 import '../styles/globals.css';
 import Header from '../components/Header';
+import { AuthProvider } from '../context/AuthContext';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
