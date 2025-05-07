7fd5bb5006431c8cb7e5314778de6fec1a843bce
mssalci <serdarsalci@hotmail.com>
2025-05-07 06:29:40 +0300

    Update [id].js

--- pages/eslesmeler/tekliflerim/[id].js
+++ pages/eslesmeler/tekliflerim/[id].js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../../../firebase/firebaseConfig';
+import { db } from '../../../firebase/firebaseConfig';
 
 export default function TeklifDetay() {
   const router = useRouter();
