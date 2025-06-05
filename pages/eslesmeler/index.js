f3614fa39efeb69539a62df5b7aa10aeee0d5666
mssalci <serdarsalci@hotmail.com>
2025-06-05 10:54:30 +0300

    Update index.js

--- pages/eslesmeler/index.js
+++ pages/eslesmeler/index.js
@@ -245,7 +245,7 @@ export default function Eslesmeler() {
           </>
         )}
       </>
-                 ) : ( 
+    ) : (
             <p className="text-sm text-yellow-600">Henüz eşleşme bulunamadı.</p>
           )}
         </li>
